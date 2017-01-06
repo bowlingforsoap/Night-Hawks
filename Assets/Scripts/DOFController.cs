03fbc62b359e232e4448ca7af4327afeae79898c
Strelchenko Vadym <bowlingforsoap@gmail.com>
2017-01-06 17:19:29 +0100

    added dynamic DOF with Mesh Colliders

---
+++ Assets/Scripts/DOFController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DOFController : MonoBehaviour {
+	public GameObject origin;
+	public GameObject target;
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray (origin.transform.position, origin.transform.forward);
+		RaycastHit hit = new RaycastHit ();
+
+		if (Physics.Raycast (ray, out hit, 1000f)) {
+			Debug.DrawRay (origin.transform.position, origin.transform.forward * 100, Color.cyan);
+
+			target.transform.position = hit.point;
+		} else {
+			Debug.DrawRay (origin.transform.position, origin.transform.forward * 100, Color.red);
+		}
+	}
+}
