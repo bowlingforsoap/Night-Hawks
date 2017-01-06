211a1bad45d98e53b4a815ce859f1f08d61b5bfd
Strelchenko Vadym <bowlingforsoap@gmail.com>
2017-01-06 15:29:06 +0100

    added first person nav. & image effects to main camera

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        // Turn off the cursor, so you don't see it on the screen.
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0f, translation);
+        
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }	
+	}
+}
