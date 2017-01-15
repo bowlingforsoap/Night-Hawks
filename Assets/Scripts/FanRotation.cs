75325cb449bcda985313b7943fa0b566f6b77b6f
Strelchenko Vadym <bowlingforsoap@gmail.com>
2017-01-15 15:40:43 +0100

    added sounds, rotating fan + radio

---
+++ Assets/Scripts/FanRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FanRotation : MonoBehaviour {
+	public float rotationSpeed;
+	public Transform movingPart;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movingPart.transform.Rotate (Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+	}
+}
