a04d57d1e8e252c303d953ce7b8d48265c814707
Strelchenko Vadym <bowlingforsoap@gmail.com>
2017-01-21 23:58:22 +0100

    recovered materials; started the girl

---
+++ Assets/RecalculateNormals.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecalculateNormals : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.RecalculateNormals();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
